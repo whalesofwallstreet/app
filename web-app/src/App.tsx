69d31dde9ac42e793c98ca4ed8728efd81af8ded
vida <victorademiju.t@gmail.com>
2026-05-08 10:14:22 +0100

    feat: init fresh Vite + React + TypeScript boilerplate structure

---
+++ web-app/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", fontFamily: "sans-serif" }}>
+      <h1>Vite + React + TypeScript Boilerplate</h1>
+    </div>
+  );
+}
