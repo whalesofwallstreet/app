af0e038a048a3f7e252f3a40d1d0c9c20c8c2c79
vida <victorademiju.t@gmail.com>
2026-05-11 10:55:03 +0100

    feat: build deposit and withdraw screen with keypad

---
+++ native-app/src/screens/WithdrawScreen.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+export default function WithdrawScreen() {
+  return (
+    <View><Text>Withdraw Screen</Text></View>
+  );
+}
